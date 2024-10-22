1007434023ee465d1d5108b5fb41ae0866d5c0ae
piemahesh <piemahesh@gmail.com>
2024-10-22 12:16:22 +0530

    db is added

--- messenger/whatsApp/urls.py
+++ messenger/whatsApp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("login/",views.homePage,name="homePage")
+    path("",views.homePage,name="homePage"),
+    path("about/",views.aboutPage,name="aboutPage"),
+    path("register/",views.register,name="registerPage"),
 ]
--- messenger/whatsApp/views.py
+++ messenger/whatsApp/views.py
@@ -1,12 +1,30 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from . import db
 # Create your views here.
 
 # function
 def homePage(req):
+    if(req.method=="POST"):
+        query = req.POST
+        userName = query.get("userName")
+        password = query.get("password")
+        user = db.userCol.find_one({"username":userName, "password":password})
+        print(user,"this is user")
+    print(req,"this is request")
     print("im called")
     return render(req, 'index.html')
 
+
+def register(req):
+    if(req.method=="POST"):
+        query = req.POST
+        userName = query.get("userName")
+        password = query.get("password")
+        confirmPassword = query.get("confirmPassword")
+        if(confirmPassword == password):
+            db.userCol.insert_one({"username":userName,"password":password})
+            return redirect("homePage")
+    return render(req,'register.html')
 def aboutPage(req):
     print("im about page")
     return render(req, 'about.html')
