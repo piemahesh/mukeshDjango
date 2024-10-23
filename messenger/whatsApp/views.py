d37f9d46b6d22b9205b928cb7de880a3620374a7
piemahesh <piemahesh@gmail.com>
2024-10-23 12:28:30 +0530

    FIX!: solve

--- messenger/whatsApp/views.py
+++ messenger/whatsApp/views.py
@@ -5,11 +5,17 @@ from . import db
 # function
 def homePage(req):
     if(req.method=="POST"):
+       
         query = req.POST
         userName = query.get("userName")
         password = query.get("password")
         user = db.userCol.find_one({"username":userName, "password":password})
-        print(user,"this is user")
+        if(not user):
+            return redirect("register")
+        else:
+            id = str(user['_id'])
+            req.session['accessKey'] = id
+            return redirect("view")
     print(req,"this is request")
     print("im called")
     return render(req, 'index.html')
@@ -23,8 +29,21 @@ def register(req):
         confirmPassword = query.get("confirmPassword")
         if(confirmPassword == password):
             db.userCol.insert_one({"username":userName,"password":password})
-            return redirect("homePage")
+            return redirect("login")
     return render(req,'register.html')
 def aboutPage(req):
     print("im about page")
     return render(req, 'about.html')
+
+def viewPage(req):
+    print("trigger")
+    users = db.userCol.find()
+    if(not req.session):
+        return redirect("login")
+    return render(req, 'view.html', {"users": users})
+    
+
+def logout(req):
+    del req.session["accessKey"]
+    print(req.session.keys())
+    return redirect("login")
